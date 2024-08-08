7b6d8768e766c4062166c692672273e7ce6132f8
kathyslate <mercyoyetunde98@gmail.com>
2024-08-08 07:17:23 -0700

    async

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Async Generator
+"""
+import random
+import asyncio
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    Coroutine async_generator
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
